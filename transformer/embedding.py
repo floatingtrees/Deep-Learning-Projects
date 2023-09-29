cba986da0b5b1d1299a588b5d72b552d627e6bcd
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2023-09-29 07:26:55 -0700

    Update embedding.py

--- transformer/embedding.py
+++ transformer/embedding.py
@@ -5,21 +5,35 @@ import time
 # matrix of word embeddings, use indexing to find the proper embedding
 
 
-class embedding:
+class Embedding:
     def __init__(self, vocab_size, features):
         self.vocab_size = vocab_size
         self.features = features
-        self.weights = np.random.rand(vocab_size, features).astype(np.float32)
+        
     def build(self):
-        pass
-    def call(self, text): # text is in shape (batch size, tokens)
-        embedded_vectors = np.zeros((text.shape[0], text.shape[1], self.features))
-        #for # use numpy advanced indexing
-        index = np.array(text)
+        self.weights = np.random.rand(vocab_size, features).astype(np.float32)
+
+    def call(self, inputs): # inputs is in shape (batch size, tokens)
+        embedded_vectors = np.zeros((inputs.shape[0], inputs.shape[1], self.features))
+        # pretty sure this works
+        index = np.array(inputs)
         sequence = np.array(self.weights[index, :])
         return sequence
 
-x = np.zeros((32, 100), dtype = np.int32)
-embed = embedding(100, 64)
-out = embed.call(x)
-print(out.shape)
+
+    def backprop(self, grads):
+        pass
+
+class Dense:
+    def __init__(self, neurons, activation):
+        self.neurons = neurons
+        self.activation = activation
+        self.biases = np.zeros((1, neurons), dtype = np.float32)
+    def build(self, dummy_input):
+        self.weights = np.random.rand(dummy_input.shape[-1], neurons).astype(np.float32)
+    def call(self, inputs):
+        return np.matmul(inputs, self.weights) + self.biases
+
+class Model:
+    def __init__(self):
+        self.Dense1 = self.
